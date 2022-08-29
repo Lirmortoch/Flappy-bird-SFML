3d9c1d098f9cb94553c2022690ae51fbe35c9c92
Lirmortoch <98690326+Lirmortoch@users.noreply.github.com>
2022-08-29 12:36:06 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -3,90 +3,24 @@
 
 int Game::loadResource()
 {
-    sf::Texture* bird = getGlobalTextureHolder().loadFromFile("Resource\\res\\birdAnimation.png", "bird");
-    if (bird == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* back = getGlobalTextureHolder().loadFromFile("Resource\\res\\background.png", "back");
-    if (back == nullptr)
-    {
-        return -1;
-
-    }
-
-    sf::Texture* ground = getGlobalTextureHolder().loadFromFile("Resource\\res\\ground.png", "ground");
-    if (ground == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* top_pipe = getGlobalTextureHolder().loadFromFile("Resource\\res\\UpperPipe.png", "top_pipe");
-    if (top_pipe == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* down_pipe = getGlobalTextureHolder().loadFromFile("Resource\\res\\DownPipe.png", "down_pipe");
-    if (down_pipe == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* startButton = getGlobalTextureHolder().loadFromFile("Resource\\res\\StartButton.png", "start_button");
-    if (startButton == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* title = getGlobalTextureHolder().loadFromFile("Resource\\res\\FB_Image.png", "title");
-    if (title == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* gameOverTitle = getGlobalTextureHolder().loadFromFile("Resource\\res\\GameOver.png", "gameOver");
-    if (gameOverTitle == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* board = getGlobalTextureHolder().loadFromFile("Resource\\res\\board.png", "board");
-    if (board == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* gold_medal = getGlobalTextureHolder().loadFromFile("Resource\\res\\goldMedal.png", "gold_medal");
-    if (gold_medal == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* silver_medal = getGlobalTextureHolder().loadFromFile("Resource\\res\\silverMedal.png", "silver_medal");
-    if (silver_medal == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* bronze_medal = getGlobalTextureHolder().loadFromFile("Resource\\res\\bronzeMedal.png", "bronze_medal");
-    if (bronze_medal == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Texture* iron_medal = getGlobalTextureHolder().loadFromFile("Resource\\res\\ironMedal.png", "iron_medal");
-    if (iron_medal == nullptr)
-    {
-        return -1;
-    }
-
-    sf::Font* text = getGlobalFontHolder().loadFromFile("Resource\\FlappyFont.ttf", "numbers");
-    if (text == nullptr)
-    {
-        return -1;
-    }
+    auto& tH = getGlobalTextureHolder();
+    auto& fH = getGlobalFontHolder();
+    
+    return
+        tH.loadFromFile("Resource\\res\\birdAnimation.png", "bird") &&
+        tH.loadFromFile("Resource\\res\\background.png", "back") &&
+        tH.loadFromFile("Resource\\res\\ground.png", "ground") &&
+        tH.loadFromFile("Resource\\res\\UpperPipe.png", "top_pipe") &&
+        tH.loadFromFile("Resource\\res\\DownPipe.png", "down_pipe") &&
+        tH.loadFromFile("Resource\\res\\StartButton.png", "start_button") &&
+        tH.loadFromFile("Resource\\res\\FB_Image.png", "title") &&
+        tH.loadFromFile("Resource\\res\\GameOver.png", "gameOver") &&
+        tH.loadFromFile("Resource\\res\\board.png", "board") &&
+        tH.loadFromFile("Resource\\res\\goldMedal.png", "gold_medal") &&
+        tH.loadFromFile("Resource\\res\\silverMedal.png", "silver_medal") &&
+        tH.loadFromFile("Resource\\res\\bronzeMedal.png", "bronze_medal") &&
+        tH.loadFromFile("Resource\\res\\ironMedal.png", "iron_medal") &&
+        fH.loadFromFile("Resource\\FlappyFont.ttf", "numbers");
 }
 
 void Game::run()
@@ -134,6 +68,10 @@ Game::Game()
     data->window.create(sf::VideoMode(windowWidth, windowHeight), gameTitle);
     data->machine.addState(StateRef(new MainMenuState(this->data)));
 
-    this->loadResource();
+    if (!this->loadResource())
+    {
+        std::cout << "Fattal error";
+    }
+    
     this->run();
 }
