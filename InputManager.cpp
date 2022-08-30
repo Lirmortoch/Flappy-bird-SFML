91dcff74755a7e944ca497f60325ca084f911bc3
Lirmortoch <98690326+Lirmortoch@users.noreply.github.com>
2022-08-30 18:53:37 +0300

    Update InputManager.cpp

--- InputManager.cpp
+++ InputManager.cpp
@@ -5,21 +5,6 @@ InputManager::InputManager(GameDataRef _data) : data(_data)
 
 }
 
-/*bool InputManager::IsSpriteClicked(sf::Sprite object, sf::Mouse::Button button)
-{
-    if (sf::Mouse::isButtonPressed(button))
-    {
-        sf::IntRect playButtonRect(object.getPosition().x, object.getPosition().y, object.getGlobalBounds().width, object.getGlobalBounds().height);
-
-        if (playButtonRect.contains(sf::Mouse::getPosition(data->window)))
-        {
-            return true;
-        }
-    }
-
-    return false;
-}*/
-
 bool InputManager::IsSpriteClicked(sf::Event event, sf::Sprite object, sf::Mouse::Button button)
 {
     if (event.type == sf::Event::MouseButtonReleased)
